54a06d2bd08821e7b1e0a9e2b0fb5364052861df
Nicholas <njbroussard.1@gmail.com>
2024-07-26 13:14:32 -0500

    add newNameHandler, newNameSubmit, resetNewName functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+
+  const [newName, setNewName] = useState('');
+
+
+  const newNameHandler = (event) => {
+    event.preventDefault();
+    setNewName(event.target.value)
+  }
+
+  const newNameSubmit = (event) => {
+    event.preventDefault();
+    persons.push({name: newName})
+    //console.log(newName)
+    setPersons(persons)
+    resetNewName()
+  }
+
+  const resetNewName = () => {
+    setNewName('')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={newNameHandler} value={newName}/>
+        </div>
+        <div>
+          <button type="submit" onClick={newNameSubmit}>add</button>
+        </div>
+        <div>
+          debug: {newName}
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        <ul>
+          {persons.map(ele=> <li key={ele.name}>{ele.name}</li>)}
+        </ul>
+    </div>
   )
 }
 

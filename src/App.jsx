284f2268f2a506e0f58ca91b35b958690e22b1ab
Nicholas <njbroussard.1@gmail.com>
2024-07-26 14:31:08 -0500

    add filter function for names

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,17 @@ const App = () => {
 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+  const [filterObj, setFilterObj] = useState({});
 
 
+  // update input name to add
   const newNameHandler = (event) => {
     event.preventDefault();
     setNewName(event.target.value)
   }
 
+//check if name to add exists
   const newNameExists = () => {
     for (let i = 0; i < persons.length; i++) {
       if (persons[i]['name'].toLowerCase() === newName.toLowerCase()) {
@@ -23,6 +27,7 @@ const App = () => {
     }
   }
 
+ // if name to add does not exists, add to perosns array of objs
   const newNameSubmit = (event) => {
     event.preventDefault();
       if (newNameExists()) {
@@ -36,26 +41,65 @@ const App = () => {
     //console.log(newName)
   }
 
+ // reset new name to add
   const resetNewName = () => {
     setNewName('');
   }
 
+ // reset new humber to add
   const resetNewNumber = () => {
     setNewNumber('');
   }
 
+ // new number handler, new number to add
   const newNumberHandler = (event) => {
     event.preventDefault();
     setNewNumber(event.target.value)
   }
 
   
+  const filterNameHandler = (event) => {
+    event.preventDefault();
+    setFilter(event.target.value);
+  }
 
+  const checkFilterExists = () => {
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i]['name'].toLowerCase() === filter.toLowerCase()) {
+        filterObj['name'] = persons[i]['name'];
+        filterObj['number'] = persons[i]['number'];
+        setFilterObj(filterObj);
+      }
+    }
+  }
+
+  const submitFilter = (event) => {
+    event.preventDefault();
+    checkFilterExists();
+    window.alert(`${filterObj['name']} : ${filterObj['number']}`)
+    resetFilter('')
+  }
+
+  const resetFilter = () => {
+    setFilter('');
+  }
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
       <form>
+        <div>
+          filter: <input onChange={filterNameHandler} value={filter}/>
+          <div>
+            <button type="submit" onClick={submitFilter}>search</button>
+          </div>
+          <div>
+            debug: {filter}
+          </div>
+        </div>
+      </form>
+      <form>
+        <h2>Add new:</h2>
         <div>
           name: <input onChange={newNameHandler} value={newName}/>
           <br></br>
@@ -70,7 +114,7 @@ const App = () => {
           debug: {newNumber}
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h2>Numbers:</h2>
         <ul>
           {persons.map(ele=> <li key={ele.name}>{ele.name ? ele.name : 'Phonebook empty'} {ele.number ? '--' + ele.number : ''}</li>)}
         </ul>
